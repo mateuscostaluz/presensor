99e1bf3858bb7d1cce1942c0f65904943e34deab
mateuscostaluz <mateuscostaluz@hotmail.com>
2020-11-18 15:34:49 -0300

    Ajuste tecnico do Request presenca

--- src/main/java/br/gov/sp/fatec/presensor/controller/PresencaController.java
+++ src/main/java/br/gov/sp/fatec/presensor/controller/PresencaController.java
@@ -1,21 +1,27 @@
 package br.gov.sp.fatec.presensor.controller;
 
-import br.gov.sp.fatec.presensor.controller.dto.PresencaRq;
 import br.gov.sp.fatec.presensor.controller.dto.PresencaRs;
+import br.gov.sp.fatec.presensor.model.HorarioDisciplina;
 import br.gov.sp.fatec.presensor.model.Presenca;
+import br.gov.sp.fatec.presensor.model.Usuario;
+import br.gov.sp.fatec.presensor.repository.HorarioDisciplinaRepository;
 import br.gov.sp.fatec.presensor.repository.PresencaRepository;
+import br.gov.sp.fatec.presensor.repository.UsuarioRepository;
 import br.gov.sp.fatec.presensor.services.DateTimeServices;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/presenca")
 public class PresencaController {
+
+    private final UsuarioRepository usuarioRepository;
+    private final HorarioDisciplinaRepository horarioDisciplinaRepository;
 
     private final PresencaRepository presencaRepository;
 
@@ -28,12 +34,33 @@ public class PresencaController {
                 .collect(Collectors.toList());
     }
 
-    @PostMapping("/")
-    public void savePresenca(@Valid @RequestBody PresencaRq presencaRq) {
+    @RequestMapping(path = "/{raUsuario}/{idHorarioDisciplina}", method = RequestMethod.POST)
+    public void savePresenca(@PathVariable("raUsuario") Long raUsuario, @PathVariable("idHorarioDisciplina") Long idHorarioDisciplina) throws Exception {
+
+        Optional<Usuario> usuario = usuarioRepository.findById(raUsuario);
+
+        Usuario usuarioSave;
+
+        if(usuario.isPresent()) {
+            usuarioSave = usuario.get();
+        } else {
+            throw new Exception("Usuário não encontrado");
+        }
+
+        Optional<HorarioDisciplina> horarioDisciplina = horarioDisciplinaRepository.findById(idHorarioDisciplina);
+
+        HorarioDisciplina horarioDisciplinaSave;
+
+        if(horarioDisciplina.isPresent()) {
+            horarioDisciplinaSave = horarioDisciplina.get();
+        } else {
+            throw new Exception("Disciplina não encontrada");
+        }
+
         Presenca presenca = new Presenca();
 
-        presenca.setUsuario(presencaRq.getUsuario());
-        presenca.setHorarioDisciplina(presencaRq.getHorarioDisciplina());
+        presenca.setUsuario(usuarioSave);
+        presenca.setHorarioDisciplina(horarioDisciplinaSave);
         presenca.setDataHora(DateTimeServices.geTimestamp());
         presencaRepository.save(presenca);
     }
