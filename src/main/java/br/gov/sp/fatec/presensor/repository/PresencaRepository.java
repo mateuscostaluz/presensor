65c33fcde9321998c4a0da27009325091ff28d11
mateuscostaluz <mateuscostaluz@hotmail.com>
2020-11-19 00:06:15 -0300

    Using CrudRepository

--- src/main/java/br/gov/sp/fatec/presensor/repository/PresencaRepository.java
+++ src/main/java/br/gov/sp/fatec/presensor/repository/PresencaRepository.java
@@ -2,9 +2,9 @@ package br.gov.sp.fatec.presensor.repository;
 
 import br.gov.sp.fatec.presensor.model.Presenca;
 import br.gov.sp.fatec.presensor.model.PresencaId;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PresencaRepository extends JpaRepository<Presenca, PresencaId> {
+public interface PresencaRepository extends CrudRepository<Presenca, PresencaId> {
 }
