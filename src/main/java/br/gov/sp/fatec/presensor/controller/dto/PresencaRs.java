bed124e5740e243c01e9bbcb6f6b30d5081e1e8b
mateuscostaluz <mateuscostaluz@hotmail.com>
2020-11-19 10:36:28 -0300

    Testing

--- src/main/java/br/gov/sp/fatec/presensor/controller/dto/PresencaRs.java
+++ src/main/java/br/gov/sp/fatec/presensor/controller/dto/PresencaRs.java
@@ -1,11 +1,6 @@
 package br.gov.sp.fatec.presensor.controller.dto;
 
-import br.gov.sp.fatec.presensor.model.HorarioDisciplina;
 import br.gov.sp.fatec.presensor.model.Presenca;
-import br.gov.sp.fatec.presensor.model.Usuario;
-import br.gov.sp.fatec.presensor.repository.HorarioDisciplinaRepository;
-import br.gov.sp.fatec.presensor.repository.UsuarioRepository;
-import br.gov.sp.fatec.presensor.services.DateTimeServices;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,26 +10,15 @@ import java.time.LocalDate;
 @Setter
 public class PresencaRs {
 
-    public static UsuarioRepository usuarioRepository;
-    public static HorarioDisciplinaRepository horarioDisciplinaRepository;
-
-    public Usuario usuario;
-    public HorarioDisciplina horarioDisciplina;
-    public LocalDate data;
+    private Long raUsuario;
+    private Long idHorarioDisciplina;
+    private LocalDate data;
 
     public static PresencaRs converter(Presenca p) {
-
         PresencaRs presencaRs = new PresencaRs();
-
-        Usuario usuario = usuarioRepository.getOne(p.getRaUsuario());
-
-        HorarioDisciplina horarioDisciplina = horarioDisciplinaRepository.getOne(p.getIdHorarioDisciplina());
-
-        LocalDate data = DateTimeServices.getLocalDate();
-
-        presencaRs.setUsuario(usuario);
-        presencaRs.setHorarioDisciplina(horarioDisciplina);
-        presencaRs.setData(data);
+        presencaRs.setRaUsuario(p.getRaUsuario());
+        presencaRs.setIdHorarioDisciplina(p.getIdHorarioDisciplina());
+        presencaRs.setData(p.getData());
         return presencaRs;
     }
 
