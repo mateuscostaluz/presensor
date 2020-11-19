866ca60841d931cba670a6f9a75eef8ccd4850e8
mateuscostaluz <mateuscostaluz@hotmail.com>
2020-11-18 21:34:55 -0300

    Composite-id class must implement Serializable

--- src/main/java/br/gov/sp/fatec/presensor/model/Presenca.java
+++ src/main/java/br/gov/sp/fatec/presensor/model/Presenca.java
@@ -4,13 +4,14 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.time.LocalDate;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "presencas")
-public class Presenca {
+public class Presenca implements Serializable {
 
     @ManyToOne
     @Id
--- src/main/java/br/gov/sp/fatec/presensor/repository/PresencaRepository.java
+++ src/main/java/br/gov/sp/fatec/presensor/repository/PresencaRepository.java
@@ -1,9 +1,8 @@
 package br.gov.sp.fatec.presensor.repository;
 
-import br.gov.sp.fatec.presensor.model.Presenca;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PresencaRepository extends JpaRepository<Presenca, Long> {
+public interface PresencaRepository extends JpaRepository {
 }
