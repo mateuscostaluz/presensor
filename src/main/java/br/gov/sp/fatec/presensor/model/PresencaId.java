e78c49f6bceb553e09a1b0a965a29e9a3db4724c
mateuscostaluz <mateuscostaluz@hotmail.com>
2020-11-18 23:59:14 -0300

    Last test before undo

--- src/main/java/br/gov/sp/fatec/presensor/model/PresencaId.java
+++ src/main/java/br/gov/sp/fatec/presensor/model/PresencaId.java
@@ -5,15 +5,13 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serializable;
-import java.time.LocalDate;
 
 @AllArgsConstructor
 @Getter
 @Setter
 public class PresencaId implements Serializable {
 
-    private Usuario usuario;
-    private HorarioDisciplina horarioDisciplina;
-    private LocalDate data;
+    private Long raUsuario;
+    private Long idHorarioDisciplina;
 
 }
