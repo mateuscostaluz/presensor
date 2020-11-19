baea7d81b0ee0f23f61bbedcb73aaf8e791cf727
mateuscostaluz <mateuscostaluz@hotmail.com>
2020-11-18 21:53:11 -0300

    Trying to solve Repository

--- src/main/java/br/gov/sp/fatec/presensor/repository/PresencaRepository.java
+++ src/main/java/br/gov/sp/fatec/presensor/repository/PresencaRepository.java
@@ -1,8 +1,9 @@
 package br.gov.sp.fatec.presensor.repository;
 
+import br.gov.sp.fatec.presensor.model.Presenca;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PresencaRepository extends JpaRepository {
+public interface PresencaRepository extends JpaRepository<Presenca, Presenca> {
 }
