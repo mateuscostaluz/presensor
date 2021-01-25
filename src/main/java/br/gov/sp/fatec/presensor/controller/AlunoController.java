9d59de69f5ed1e3b5667fc68bb22a3bf692f6127
mateuscostaluz <mateuscostaluz@hotmail.com>
2021-01-25 18:17:49 -0300

    @RequestBody

--- src/main/java/br/gov/sp/fatec/presensor/controller/AlunoController.java
+++ src/main/java/br/gov/sp/fatec/presensor/controller/AlunoController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,7 @@ public class AlunoController {
     private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/")
-    public ResponseEntity<AlunoRq> saveAluno(AlunoRq alunoRq) {
+    public ResponseEntity<AlunoRq> saveAluno(@RequestBody AlunoRq alunoRq) {
 
         System.out.println("AlunoRq: " + alunoRq.getEmail());
 
