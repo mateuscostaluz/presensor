61027b14722ddbaaf8be75ce259b6d64fb779fa6
mateuscostaluz <mateuscostaluz@hotmail.com>
2021-01-25 17:47:12 -0300

    Register without auth

--- src/main/java/br/gov/sp/fatec/presensor/security/WebSecurityConfig.java
+++ src/main/java/br/gov/sp/fatec/presensor/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package br.gov.sp.fatec.presensor.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -36,6 +37,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
+        .antMatchers(HttpMethod.POST, "/aluno/").permitAll()
         .anyRequest().authenticated()
         .and().formLogin().permitAll()
         .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
