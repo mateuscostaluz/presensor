dcf2d0023aa33ee6a37f7ca39c28deb9f9c7e30a
mateuscostaluz <mateuscostaluz@hotmail.com>
2021-01-25 18:21:48 -0300

    teste

--- src/main/java/br/gov/sp/fatec/presensor/repository/AlunoRepository.java
+++ src/main/java/br/gov/sp/fatec/presensor/repository/AlunoRepository.java
@@ -1,18 +1,12 @@
 package br.gov.sp.fatec.presensor.repository;
 
 import br.gov.sp.fatec.presensor.model.Aluno;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AlunoRepository extends CrudRepository<Aluno, Long> {
 
-    @Query(value = "SELECT * FROM alunos " +
-                   "WHERE email = :email ", nativeQuery = true)
-    Aluno findByEmail(
-            @Param("email") String email
-    );
+    Aluno findByEmail(String email);
 
 }
