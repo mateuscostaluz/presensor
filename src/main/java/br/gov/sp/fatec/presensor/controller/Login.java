b0e6c5841864c73f8064f1973bcca8d4cb464cb0
mateuscostaluz <mateuscostaluz@hotmail.com>
2021-01-25 19:30:34 -0300

    testing

--- src/main/java/br/gov/sp/fatec/presensor/controller/Login.java
+++ src/main/java/br/gov/sp/fatec/presensor/controller/Login.java
@@ -1,16 +1,8 @@
 package br.gov.sp.fatec.presensor.controller;
 
-import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@AllArgsConstructor
 @RestController
 @RequestMapping("/")
-public class Login {
-
-    @GetMapping("")
-    public void login() {}
-
-}
+public class Login {}
