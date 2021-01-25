48ea95ce2329947f20a97aef60193d1ebd9d280e
mateuscostaluz <mateuscostaluz@hotmail.com>
2021-01-25 17:51:22 -0300

    add s on aluno for have alunos /tcharam /facepalm

--- src/main/java/br/gov/sp/fatec/presensor/security/WebSecurityConfig.java
+++ src/main/java/br/gov/sp/fatec/presensor/security/WebSecurityConfig.java
@@ -37,7 +37,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
-        .antMatchers(HttpMethod.POST, "/aluno/").permitAll()
+        .antMatchers(HttpMethod.POST, "/alunos/").permitAll()
         .anyRequest().authenticated()
         .and().formLogin().permitAll()
         .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
