c839abdff2c06b982452b1f5bd39a53d7fac8139
mateuscostaluz <mateuscostaluz@hotmail.com>
2021-02-08 00:20:42 -0300

    removing not use dependencies³

--- src/main/java/br/gov/sp/fatec/presensor/Presensor.java
+++ src/main/java/br/gov/sp/fatec/presensor/Presensor.java
@@ -1,10 +1,8 @@
 package br.gov.sp.fatec.presensor;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = "br.gov.sp.fatec.presensor")
 @EntityScan(basePackages = "br.gov.sp.fatec.presensor.model")
@@ -12,11 +10,6 @@ public class Presensor {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Presensor.class, args);
-	}
-
-	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
 	}
 
 }
