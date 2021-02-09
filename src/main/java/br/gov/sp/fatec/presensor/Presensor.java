e694cad92147abb3c32913d949ebfac3b797eede
mateuscostaluz <mateuscostaluz@hotmail.com>
2021-02-08 21:37:18 -0300

    enabled

--- src/main/java/br/gov/sp/fatec/presensor/Presensor.java
+++ src/main/java/br/gov/sp/fatec/presensor/Presensor.java
@@ -3,9 +3,11 @@ package br.gov.sp.fatec.presensor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication(scanBasePackages = "br.gov.sp.fatec.presensor")
 @EntityScan(basePackages = "br.gov.sp.fatec.presensor.model")
+@EnableGlobalMethodSecurity(proxyTargetClass = true, prePostEnabled = true)
 public class Presensor {
 
 	public static void main(String[] args) {
--- src/main/java/br/gov/sp/fatec/presensor/controller/AlunoController.java
+++ src/main/java/br/gov/sp/fatec/presensor/controller/AlunoController.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,7 +20,6 @@ import java.util.stream.Collectors;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/aluno")
-@EnableGlobalMethodSecurity(proxyTargetClass = true)
 public class AlunoController {
 
     @Autowired
