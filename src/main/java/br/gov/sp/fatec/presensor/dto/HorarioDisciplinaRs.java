4a7147862b297143382418aa4ec9466704c7e13e
mateuscostaluz <mateuscostaluz@hotmail.com>
2021-06-04 01:31:24 -0300

    json rename

--- src/main/java/br/gov/sp/fatec/presensor/dto/HorarioDisciplinaRs.java
+++ src/main/java/br/gov/sp/fatec/presensor/dto/HorarioDisciplinaRs.java
@@ -1,6 +1,7 @@
 package br.gov.sp.fatec.presensor.dto;
 
 import br.gov.sp.fatec.presensor.model.HorarioDisciplina;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,12 +9,19 @@ import lombok.Setter;
 @Setter
 public class HorarioDisciplinaRs {
 
+    @JsonProperty(value = "id_horario_disciplina")
     private Long id;
+    @JsonProperty(value = "sigla_disciplina")
     private String siglaDisciplina;
+    @JsonProperty(value = "nome_disciplina")
     private String nomeDisciplina;
+    @JsonProperty(value = "numero_sala")
     private Integer numeroSala;
+    @JsonProperty(value = "dia_semana")
     private String diaSemana;
+    @JsonProperty(value = "horario_inicio_disciplina")
     private String horarioInicio;
+    @JsonProperty(value = "horario_fim_disciplina")
     private String horarioFim;
 
     public static HorarioDisciplinaRs converter(HorarioDisciplina hd) {
